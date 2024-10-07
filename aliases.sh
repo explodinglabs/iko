a08e0d80f9f2e3295d545ce1079bc3817c6b38d5
Beau Barker <beau@explodinglabs.com>
2024-10-07 20:20:54 +1100

    Adjustments

--- aliases.sh
+++ aliases.sh
@@ -7,22 +7,22 @@ function show-files {
 
 function create-schema {
     local name=$1
-    set -e
-    ./sqitch add create_schema_$name \
+    local change_name=${2:-create_schema_$name}
+    ./sqitch add $change_name \
         --template create_schema \
         --set name=$name \
-        --note \'"Create $name schema"\'
-    show-files create_schema_$name
+        --note \'"Create $name schema"\' \
+    && show-files create_schema_$name
 }
 
 function drop-schema {
     local name=$1
-    set -e
-    ./sqitch add drop_schema_$name \
+    local change_name=${2:-drop_schema_$name}
+    ./sqitch add $change_name \
         --template drop_schema \
         --set name=$name \
-        --note \'"Drop $name schema"\'
-    show-files drop_schema_$name
+        --note \'"Drop $name schema"\' \
+    && show-files drop_schema_$name
 }
 
 # Tables
@@ -30,25 +30,23 @@ function drop-schema {
 function create-table {
     local schema_name=$1
     local table_name=$2
-    set -e
-    ./sqitch add create_table_${schema_name}_${table_name} \
+    local change_name=${3:-create_table_${schema_name}_${table_name}}
+    ./sqitch add $change_name \
         --template create_table \
         --set schema=$schema_name \
         --set name=$table_name \
-        --note \'"Create ${schema_name}.${table_name} table"\'
-    show-files create_table_${schema_name}_${table_name}
+        --note \'"Create ${schema_name}.${table_name} table"\' \
+    && show-files create_table_${schema_name}_${table_name}
 }
 
 function drop-table {
-    local db_name=$1
-    local schema_name=$2
-    local table_name=$3
-    set -e
+    local schema_name=$1
+    local table_name=$2
+    local change_name=${3:-drop_table_${schema_name}_${table_name}}
     ./sqitch add drop_table_${schema_name}_${table_name} \
         --template drop_table \
-        --set schema=$schema_name \
-        --set name=$table_name \
-        --set recreate=$(docker exec -it postgres pg_dump -t $schema_name.$table_name --schema-only $db_name) \
-        --note \'"Drop ${schema_name}.${table_name} table"\'
-    show-files drop_table_${schema_name}_${table_name}
+        --set schema_name=$schema_name \
+        --set table_name=$table_name \
+        --note \'"Drop ${schema_name}.${table_name} table"\' \
+    && show-files drop_table_${schema_name}_${table_name}
 }
